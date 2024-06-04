da9da99ce75a13ed6b6138acf13c4874fff5b3d3
Myst33d <notmyst33d@gmail.com>
2024-06-04 18:40:47 +0600

    Add new ez

--- ez.h
+++ ez.h
@@ -10,13 +10,25 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+#ifndef CC
+#define CC "cc"
+#endif
+
+#ifndef THREADS
+#define THREADS 16
+#endif
+
 #define BLUE "\x1b[34m"
 #define RED "\x1b[31m"
 #define INFO(message, args...) printf("\x1b[1m[%s%s\x1b[0;1m]\x1b[0m " message"\n", BLUE, "INFO", ##args);
 #define ERROR(message, args...) printf("\x1b[1m[%s%s\x1b[0;1m]\x1b[0m " message"\n", RED, "ERROR", ##args);
+#define assert(expr) if (expr != 0) return -1
 
-#define MAX_THREADS 16
-pthread_t thread_pool[MAX_THREADS] = { 0 };
+void ez_mkdir(char *path) {
+    mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+}
+
+pthread_t global_thread_pool[THREADS] = { 0 };
 
 int fail = 0;
 pthread_mutex_t fail_mutex;
@@ -81,6 +93,8 @@ typedef struct {
     pthread_cond_t cond;
     Command **cmds;
     size_t size;
+    int working_threads;
+    pthread_mutex_t working_threads_mutex;
 } CommandQueue;
 
 CommandQueue global_queue = { 0 };
@@ -105,12 +119,16 @@ Command *queue_pop(CommandQueue *queue) {
     return cmd;
 }
 
-void *queue_worker(void*) {
+void *queue_worker(void *queue_ptr) {
+    CommandQueue *queue = queue_ptr;
     while (!fail) {
-        Command *cmd = queue_pop(&global_queue);
+        Command *cmd = queue_pop(queue);
         if (cmd == 0) {
             break;
         }
+        pthread_mutex_lock(&queue->working_threads_mutex);
+        queue->working_threads++;
+        pthread_mutex_unlock(&queue->working_threads_mutex);
         INFO("%s", cmd->message);
         int status = cmd_execute(cmd);
         if (status) {
@@ -119,22 +137,95 @@ void *queue_worker(void*) {
             pthread_mutex_unlock(&fail_mutex);
         }
         cmd_free(cmd);
+        pthread_mutex_lock(&queue->working_threads_mutex);
+        queue->working_threads--;
+        pthread_mutex_unlock(&queue->working_threads_mutex);
     }
     return 0;
 }
 
-void queue_shutdown(void) {
-    for (int i = 0; i < MAX_THREADS; i++) {
-        queue_push(&global_queue, 0);
+void queue_shutdown(CommandQueue *queue, pthread_t *thread_pool, int thread_count) {
+    for (int i = 0; i < thread_count; i++) {
+        queue_push(queue, 0);
     }
-
-    for (int i = 0; i < MAX_THREADS; i++) {
+    for (int i = 0; i < thread_count; i++) {
         pthread_join(thread_pool[i], 0);
     }
 }
 
-void ez_mkdir(char *path) {
-    mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+int queue_wait(CommandQueue *queue, pthread_t *thread_pool, int thread_count) {
+    while ((queue->size > 0 || queue->working_threads > 0) && !fail) {
+        usleep(1000);
+    }
+    if (fail) {
+        ERROR("Compilation aborted");
+        queue_shutdown(queue, thread_pool, thread_count);
+        return -1;
+    }
+    return 0;
 }
 
-#define assert(expr) if (expr != 0) return -1;
+void queue_start(CommandQueue *queue, pthread_t *thread_pool, int thread_count) {
+    for (int i = 0; i < thread_count; i++) {
+        pthread_create(&thread_pool[i], 0, queue_worker, queue);
+    }
+}
+
+#define global_queue_push(cmd) queue_push(&global_queue, cmd)
+#define global_queue_start() queue_start(&global_queue, global_thread_pool, THREADS)
+#define global_queue_wait() queue_wait(&global_queue, global_thread_pool, THREADS)
+#define global_queue_shutdown() queue_shutdown(&global_queue, global_thread_pool, THREADS)
+
+void queue_cc(CommandQueue *queue, char *in, char *out, char **flags) {
+    Command *cmd = cmd_new();
+    char message[512];
+
+    sprintf(message, CC " %s", in);
+    cmd_message(cmd, message);
+
+    cmd_push(cmd, CC);
+    for (int i = 0; flags[i] != 0; i++) {
+        cmd_push(cmd, flags[i]);
+    }
+    cmd_push(cmd, "-c");
+    cmd_push(cmd, "-o");
+    cmd_push(cmd, out);
+    cmd_push(cmd, in);
+
+    queue_push(queue, cmd);
+}
+
+void queue_ld(CommandQueue *queue, char *out, char **objs, char **flags) {
+    Command *cmd = cmd_new();
+    char message[512];
+
+#ifdef USE_LD
+    sprintf(message, LD " %s", out);
+#else
+    sprintf(message, "ld %s", out);
+#endif
+    cmd_message(cmd, message);
+
+    cmd_push(cmd, CC);
+#ifdef USE_LD
+    cmd_push(cmd, "-fuse-ld=" LD);
+#endif
+    for (int i = 0; flags[i] != 0; i++) {
+        cmd_push(cmd, flags[i]);
+    }
+    cmd_push(cmd, "-o");
+    cmd_push(cmd, out);
+    for (int i = 0; objs[i] != 0; i++) {
+        cmd_push(cmd, objs[i]);
+    }
+
+    queue_push(queue, cmd);
+}
+
+void global_queue_cc(char *in, char *out, char **flags) {
+    queue_cc(&global_queue, in, out, flags);
+}
+
+void global_queue_ld(char *out, char **objs, char **flags) {
+    queue_ld(&global_queue, out, objs, flags);
+}
--- ezfile.c
+++ ezfile.c
@@ -1,29 +1,15 @@
 //usr/bin/cc -x c -O2 -Wall -lpthread -o .buildbin "$0" && ./.buildbin; exit
 
+#define CC "clang"
+#define USE_LD
+#define LD "lld"
 #include "ez.h"
 
-#define CC "clang"
-#define LD "lld"
+char *compile_flags[] = { "-Wall", "-Iraylib/src", "-Iraygui/src", "-DPLATFORM_DESKTOP", 0 };
+char *binary[] = { "-lm", "-lglfw", 0 };
 
-void cc(char *in, char *out) {
-    Command *cmd = cmd_new();
-    char message[512];
-
-    sprintf(message, CC " %s", in);
-    cmd_message(cmd, message);
-
-    cmd_push(cmd, CC);
-    cmd_push(cmd, "-Wall");
-    cmd_push(cmd, "-Iraylib/src");
-    cmd_push(cmd, "-Iraygui/src");
-    cmd_push(cmd, "-DPLATFORM_DESKTOP");
-    cmd_push(cmd, "-c");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, out);
-    cmd_push(cmd, in);
-
-    queue_push(&global_queue, cmd);
-}
+#define cc(in, out) global_queue_cc(in, out, compile_flags)
+#define ld global_queue_ld
 
 void build_raylib(void) {
     cc("raylib/src/rcore.c", "out/raylib/rcore.o");
@@ -38,45 +24,30 @@ void build_main(void) {
 }
 
 int link_main(void) {
-    Command *cmd = cmd_new();
-    cmd_push(cmd, CC);
-    cmd_push(cmd, "-fuse-ld=" LD);
-    cmd_push(cmd, "-lm");
-    cmd_push(cmd, "-lglfw");
-    cmd_push(cmd, "-o");
-    cmd_push(cmd, "out/main");
-    cmd_push(cmd, "out/main.o");
-    cmd_push(cmd, "out/raylib/rcore.o");
-    cmd_push(cmd, "out/raylib/rshapes.o");
-    cmd_push(cmd, "out/raylib/rtext.o");
-    cmd_push(cmd, "out/raylib/rtextures.o");
-    cmd_push(cmd, "out/raylib/utils.o");
-    assert(cmd_execute(cmd));
-    cmd_free(cmd);
-    return 0;
+    ld("out/main", (char*[]){
+        "out/main.o",
+        "out/raylib/rcore.o",
+        "out/raylib/rshapes.o",
+        "out/raylib/rtext.o",
+        "out/raylib/rtextures.o",
+        "out/raylib/utils.o", 0
+    }, binary);
 }
 
 int main(void) {
     ez_mkdir("out");
     ez_mkdir("out/raylib");
 
-    for (int i = 0; i < MAX_THREADS; i++) {
-        pthread_create(&thread_pool[i], 0, queue_worker, 0);
-    }
+    global_queue_start();
 
     build_raylib();
     build_main();
+    assert(global_queue_wait());
 
-    while (global_queue.size > 0 && !fail) {
-        usleep(1000);
-    }
-    queue_shutdown();
-    if (fail) {
-        ERROR("Compilation aborted");
-        return -1;
-    }
+    link_main();
+    assert(global_queue_wait());
 
-    assert(link_main());
+    global_queue_shutdown();
 
     INFO("Done");
     return 0;
